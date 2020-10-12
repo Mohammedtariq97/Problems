8a30f49dc1f782322a7acbf4f39a4ce1d2205e53
Mohammedtariq97 <68746178+Mohammedtariq97@users.noreply.github.com>
2020-10-12 12:25:18 +0530

    maxConsecutiveOnes added

---
+++ Arrays_Strings/maxConsecutiveOnes.java
@@ -0,0 +1,22 @@
+
+public class maxConsecutiveOnes {
+
+	public static void main(String[] args) {
+		int[] nums = { 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1 };
+		System.out.println(maxConsecutiveOnes(nums));
+	}
+	
+	private static int maxConsecutiveOnes(int[] nums) {
+		int count = 0;
+		int maxCount = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 1) {
+				count++;
+			} else {
+				maxCount = Math.max(maxCount, count);
+				count = 0;
+			}
+		}
+		return Math.max(maxCount, count);
+	}
+}
