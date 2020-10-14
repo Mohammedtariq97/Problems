26805f6f36aa51a1ebc28c46da7f2d660858b123
Mohammedtariq97 <68746178+Mohammedtariq97@users.noreply.github.com>
2020-10-14 10:26:19 +0530

    Duplicate added

---
+++ Arrays_Strings/duplicateNumbers.java
@@ -0,0 +1,23 @@
+public class duplicateNumbers {
+
+	public static void main(String[] args) {
+		int[] nums = {0,0,1,1,1,2,2,3,3,4};
+		int len = removeDuplicates(nums);
+		for(int i=0;i<len;i++) {
+		System.out.println(nums[i]);
+		}
+	}
+
+	private static int removeDuplicates(int[] nums) {
+		 int j;
+	     int k=1;
+	     for(int i=0;i<nums.length-1;i++){
+	         j=i+1;
+	         if(nums[i]!=nums[j]){
+	             nums[k]=nums[j];
+	             k++;
+	         } 
+	     }
+	     return k;
+	}
+}
