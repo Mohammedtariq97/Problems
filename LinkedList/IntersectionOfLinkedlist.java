4f620ff4e5def2bb02593abcb5f5d76d155f6e11
Mohammedtariq97 <68746178+Mohammedtariq97@users.noreply.github.com>
2020-10-27 13:14:14 +0530

    Intersection formatted

--- LinkedList/IntersectionOfLinkedlist.java
+++ LinkedList/IntersectionOfLinkedlist.java
@@ -1,7 +1,7 @@
 import java.util.LinkedList;
 
 public class IntOfLinkedList {
-	  static ListNode head1,head2; 
+	  static ListNode head1,head2;
 	  static class ListNode {
 		    int val;
 		    ListNode next;
@@ -9,46 +9,46 @@ public class IntOfLinkedList {
 		        val = x;
 		        next = null;
 		    }
-		}  
-	  
-	  public static void main(String[] args) {	
+		}
+
+	  public static void main(String[] args) {
 		IntOfLinkedList list = new IntOfLinkedList();
-		
-		list.head1 = new ListNode(4); 
-        list.head1.next = new ListNode(1); 
-        list.head1.next.next = new ListNode(8); 
-        list.head1.next.next.next = new ListNode(4); 
-        list.head1.next.next.next.next = new ListNode(5); 
-             
-        list.head2 = new ListNode(5); 
-        list.head2.next = new ListNode(6); 
-        list.head2.next.next = new ListNode(1); 
-        list.head2.next.next = new ListNode(8); 
-        list.head2.next.next.next = new ListNode(4); 
-        list.head2.next.next.next.next = new ListNode(5); 
-        
+
+		list.head1 = new ListNode(4);
+        list.head1.next = new ListNode(1);
+        list.head1.next.next = new ListNode(8);
+        list.head1.next.next.next = new ListNode(4);
+        list.head1.next.next.next.next = new ListNode(5);
+
+        list.head2 = new ListNode(5);
+        list.head2.next = new ListNode(6);
+        list.head2.next.next = new ListNode(1);
+        list.head2.next.next = new ListNode(8);
+        list.head2.next.next.next = new ListNode(4);
+        list.head2.next.next.next.next = new ListNode(5);
+
         list.head1.next=list.head2.next.next;
-        
+
         ListNode resNode = getIntersectionNode(head1,head2);
         System.out.println(resNode.val);
-	}
-	  
+	 }
+
 	  public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-	        if(headA==null || headB == null) return null;      
+	        if(headA==null || headB == null) return null;
 	        ListNode a_pointer = headA;
-	        ListNode b_pointer = headB;       
-	        while(a_pointer != b_pointer){           
+	        ListNode b_pointer = headB;
+	        while(a_pointer != b_pointer){
 	            if(a_pointer == null){
 	                a_pointer = headB;
 	            }else{
 	                a_pointer = a_pointer.next;
-	            }            
+	            }
 	            if(b_pointer == null){
 	                b_pointer = headA;
 	            }else{
 	                b_pointer = b_pointer.next;
-	            }  
+	            }
 	        }
-	       return a_pointer; 
-	    } 
+	       return a_pointer;
+	    }
 }
