d7c39161f18ec3e1eef3f32f985c6fa86597fcbb
Mohammedtariq97 <68746178+Mohammedtariq97@users.noreply.github.com>
2020-11-02 13:44:41 +0530

    Formatted

--- LinkedList/RemoveElements.java
+++ LinkedList/RemoveElements.java
@@ -1,44 +1,45 @@
 public class RemoveElements {
-	static ListNode head;  
-	  static class ListNode {
-		    int val;
-		    ListNode next;
-		    ListNode(int x) {
-		        val = x;
-		        next = null;
-		    }
-	  }
+	static ListNode head;
+
+	static class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+			next = null;
+		}
+	}
 
 	public static void main(String[] args) {
 		RemoveElements list = new RemoveElements();
-		list.head = new ListNode(1); 
-        list.head.next = new ListNode(2); 
-        list.head.next.next = new ListNode(6); 
-        list.head.next.next.next = new ListNode(3); 
-        list.head.next.next.next.next = new ListNode(4);  
-        list.head.next.next.next.next.next = new ListNode(5); 
-        list.head.next.next.next.next.next.next= new ListNode(6);  
-        
-        ListNode current = removeElements(head,6);
-        while(current!=null) {
-        	System.out.println(current.val);
-        	current = current.next;
-        }  
+		list.head = new ListNode(1);
+		list.head.next = new ListNode(2);
+		list.head.next.next = new ListNode(6);
+		list.head.next.next.next = new ListNode(3);
+		list.head.next.next.next.next = new ListNode(4);
+		list.head.next.next.next.next.next = new ListNode(5);
+		list.head.next.next.next.next.next.next = new ListNode(6);
+
+		ListNode current = removeElements(head, 6);
+		while (current != null) {
+			System.out.println(current.val);
+			current = current.next;
+		}
 	}
 
 	private static ListNode removeElements(ListNode head, int val) {
 		while (head != null && head.val == val) {
-            head = head.next;
-        }
-        ListNode dummy = head;
-        while (dummy != null && dummy.next != null) {
-            if (dummy.next.val == val) {
-                dummy.next = dummy.next.next;
-            } else {
-                dummy = dummy.next;
-            }
-        }
-        return head;
-    }
-
+			head = head.next;
+		}
+		ListNode dummy = head;
+		while (dummy != null && dummy.next != null) {
+			if (dummy.next.val == val) {
+				dummy.next = dummy.next.next;
+			} else {
+				dummy = dummy.next;
+			}
+		}
+		return head;
+	}
 }
--- LinkedList/oddEvenList.java
+++ LinkedList/oddEvenList.java
@@ -1,45 +1,46 @@
-import RemoveElements.ListNode;
-
 public class oddEvenList {
 	static ListNode head;
-	  static class ListNode {
-		    int val;
-		    ListNode next;
-		    ListNode(int x) {
-		        val = x;
-		        next = null;
-		    }
-	  }
+
+	static class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+			next = null;
+		}
+	}
 
 	public static void main(String[] args) {
 		oddEvenList list = new oddEvenList();
 		list.head = new ListNode(1);
-        list.head.next = new ListNode(2);
-        list.head.next.next = new ListNode(3);
-        list.head.next.next.next = new ListNode(4);
-        list.head.next.next.next.next = new ListNode(5);
-        list.head.next.next.next.next.next = new ListNode(6);
-        list.head.next.next.next.next.next.next= new ListNode(7);
-        list.head.next.next.next.next.next.next.next= new ListNode(8);     
-        ListNode current = oddEvenlist(head);
-        while(current!=null) {
-        	System.out.println(current.val);
-        	current = current.next;
-        }
+		list.head.next = new ListNode(2);
+		list.head.next.next = new ListNode(3);
+		list.head.next.next.next = new ListNode(4);
+		list.head.next.next.next.next = new ListNode(5);
+		list.head.next.next.next.next.next = new ListNode(6);
+		list.head.next.next.next.next.next.next = new ListNode(7);
+		list.head.next.next.next.next.next.next.next = new ListNode(8);
+		ListNode current = oddEvenlist(head);
+		while (current != null) {
+			System.out.println(current.val);
+			current = current.next;
+		}
 	}
 
 	private static ListNode oddEvenlist(ListNode head) {
-		if(head==null) return head;
-		ListNode odd=head;
+		if (head == null)
+			return head;
+		ListNode odd = head;
 		ListNode evenHead = head.next;
 		ListNode even = evenHead;
-		while(even!=null && even.next!=null) {
-			odd.next=even.next;
-			odd=odd.next;
-			even.next=odd.next;
-			even=even.next;
+		while (even != null && even.next != null) {
+			odd.next = even.next;
+			odd = odd.next;
+			even.next = odd.next;
+			even = even.next;
 		}
-		odd.next=evenHead;
-        return head;
+		odd.next = evenHead;
+		return head;
 	}
 }
